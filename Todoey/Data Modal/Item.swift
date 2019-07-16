943fc7f09b103775283a13a1c6753144c21e17ec
Manu <manugowda.nr@gmail.com>
2019-07-16 15:15:54 +0530

    implementation of mvc pattern in my app

---
+++ Todoey/Data Modal/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Manu Gowda N R on 7/16/19.
+//  Copyright © 2019 i-exceed. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var done = false
+}
